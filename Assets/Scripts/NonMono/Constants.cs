1f09f9ac560301d08c86a164280e96a092d3ec0e
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-23 19:39:23 +0300

    Proper cell size in the Constants.cs

--- Assets/Scripts/NonMono/Constants.cs
+++ Assets/Scripts/NonMono/Constants.cs
@@ -1,6 +1,6 @@
 public struct Constants
 {
-    public const int TERRAIN_CELL_SIZE = 10;       // Ячейка - квадрат. Сторона квадрата = 1
+    public const int TERRAIN_CELL_SIZE = 10;       // Ячейка - квадрат. Сторона квадрата = 10 - 
     public const int LEVEL_WIDTH = 10;            // Ширина игрового поля в ячейках
     public const int LEVEL_HEIGHT = 10;           // Высота игрового поля в ячейках
 }
