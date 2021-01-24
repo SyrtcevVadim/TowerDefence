c6b761b30ba8baabda70a00ec246f4fd1aa20283
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-24 21:58:47 +0300

    Enemies moves along provided paths

--- Assets/Scripts/MovingPath.cs
+++ Assets/Scripts/MovingPath.cs
@@ -1,18 +1,45 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MovingPath : MonoBehaviour
 {
-    public Transform[] pathLinks;
+    public Transform[] PathLinks;
+    public int TargetLinkIndex = 0;           // Индекс точки, к которой двигается текущий объект
     public void OnDrawGizmos()
     {
-        if(pathLinks == null || pathLinks.Length == 0)
+        if(PathLinks == null || PathLinks.Length == 0)
         {
             return;
         }
-        for(int i = 1; i < pathLinks.Length; i++)
+        for(int i = 1; i < PathLinks.Length; i++)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawLine(pathLinks[i - 1].position, pathLinks[i].position);
+            Gizmos.DrawLine(PathLinks[i - 1].position, PathLinks[i].position);
+        }
+    }
+
+    public IEnumerator<Transform> GetNextPathLink()
+    {
+        if (PathLinks.Length == 0 || PathLinks == null)
+        {
+            yield break;
+        }
+        
+        while(true)
+        {
+            print("Следующая точка: " + PathLinks[TargetLinkIndex].name);
+            yield return PathLinks[TargetLinkIndex];                            // Вызывается при вызове функции MoveNext() из итератора
+
+            TargetLinkIndex += 1;
+            if(PathLinks.Length == TargetLinkIndex)
+            {
+                break;
+            }
+            if (PathLinks.Length == 1)
+            {
+                continue;
+            }
+
         }
     }
 }
