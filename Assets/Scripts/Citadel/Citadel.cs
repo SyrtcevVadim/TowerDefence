c50f4c191383315b79c82598eb666c099e791132
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-24 17:13:18 +0300

    Citadel.cs stores Citadels' health

---
+++ Assets/Scripts/Citadel/Citadel.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Citadel : MonoBehaviour
+{
+    [Tooltip("Health of the citadel")]
+    public int Health;
+}
