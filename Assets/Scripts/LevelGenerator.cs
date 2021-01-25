80ae362f06db4359aa0cab2981d3d2332bc5e761
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-25 10:05:00 +0300

    LevelReorganization

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject LevelField;           // Parent object for all Terrain objects
     public GameObject Terrain;              // Terrain platform prefab
 
     public Vector3 StartPosition;           // Top left corner coordinates of field
     public Vector3 CurrentPosition;         // Current platform position
+
+    public GameObject CitadelPrefab;              // Player's main building
     public void Awake()
     {
         StartPosition = Vector3.zero;
         CurrentPosition = Vector3.zero;
-        //CreateFields();
+        CreateFields();
+        CreateCitadel();
     }
     [ContextMenu("CreateField")]
     public void CreateFields()
@@ -20,12 +22,27 @@ public class LevelGenerator : MonoBehaviour
         {
             for(int j = 0; j< Constants.LEVEL_WIDTH; j++)
             {
-                GameObject newTerrain = Instantiate(Terrain, CurrentPosition, Quaternion.identity);
-                newTerrain.transform.SetParent(LevelField.transform);
-                CurrentPosition += new Vector3(0, 0, -1 * Constants.TERRAIN_CELL_SIZE);
+                GameObject newTerrain = Instantiate(Terrain, CurrentPosition, Quaternion.identity, transform);
+                CurrentPosition += new Vector3(0, 0, 1 * Constants.TERRAIN_CELL_SIZE);
             }
             CurrentPosition.z = StartPosition.z;
-            CurrentPosition += new Vector3(-1 * Constants.TERRAIN_CELL_SIZE, 0, 0);
+            CurrentPosition += new Vector3(1 * Constants.TERRAIN_CELL_SIZE, 0, 0);
         }
+    }
+    public void CreateCitadel()
+    {
+        Instantiate(CitadelPrefab, new Vector3(4 * Constants.TERRAIN_CELL_SIZE, CitadelPrefab.transform.localScale.y/2, Constants.LEVEL_HEIGHT*Constants.TERRAIN_CELL_SIZE), Quaternion.identity);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Vector3 bottomLeftCorner = Vector3.zero;
+        Vector3 topLeftCorner = new Vector3(0, 0, Constants.TERRAIN_CELL_SIZE * Constants.LEVEL_HEIGHT);
+        Vector3 bottomRightCorner = new Vector3(Constants.LEVEL_WIDTH * Constants.TERRAIN_CELL_SIZE, 0, 0);
+        Vector3 topRightCorner = new Vector3(Constants.LEVEL_WIDTH * Constants.TERRAIN_CELL_SIZE, 0, Constants.TERRAIN_CELL_SIZE * (Constants.LEVEL_HEIGHT-1));
+        Gizmos.DrawLine(bottomLeftCorner, topLeftCorner);
+        Gizmos.DrawLine(bottomLeftCorner, bottomRightCorner);
+        Gizmos.DrawLine(topLeftCorner, topRightCorner);
+        Gizmos.DrawLine(topRightCorner, bottomRightCorner);
     }
 }
