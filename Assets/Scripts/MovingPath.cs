c81a52947db53e4d92199a5549bfce9bb8c65f81
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-29 19:11:12 +0300

    Added two path objects

--- Assets/Scripts/MovingPath.cs
+++ Assets/Scripts/MovingPath.cs
@@ -18,20 +18,4 @@ public class MovingPath : MonoBehaviour
             return PathLinks.Length;
         }
     }
-
-    ///// <summary>
-    ///// Отрисовывает путь в редакторе
-    ///// </summary>
-    //public void OnDrawGizmos()
-    //{
-    //    if(PathLinks == null || PathLinks.Length == 0)
-    //    {
-    //        return;
-    //    }
-    //    for(int i = 1; i < PathLinks.Length; i++)
-    //    {
-    //        Gizmos.color = Color.red;
-    //        Gizmos.DrawLine(PathLinks[i - 1].position, PathLinks[i].position);
-    //    }
-    //}
 }
--- Assets/Scripts/NonMono/Colors.cs
+++ Assets/Scripts/NonMono/Colors.cs
@@ -5,6 +5,6 @@ public class Colors
     /// <summary>
     /// Цвета для путей уровня. Каждый путь обозначается своим цветом
     /// </summary>
-    public static Color[] colors = new Color[9] { Color.white, Color.black, Color.green, Color.yellow, Color.blue, Color.cyan, Color.magenta, Color.grey, Color.red};
+    public static Color[] colors = new Color[8] { Color.red, Color.green, Color.yellow, Color.blue, Color.magenta, Color.cyan, Color.grey,Color.black};
     
 }
