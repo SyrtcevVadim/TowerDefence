5180f389f38fcd6f8856948953d4c3a60b55a0aa
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-29 18:52:52 +0300

    PathManager.cs, which stores all pathes of the level

--- Assets/Scripts/MovingPath.cs
+++ Assets/Scripts/MovingPath.cs
@@ -19,19 +19,19 @@ public class MovingPath : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Отрисовывает путь в редакторе
-    /// </summary>
-    public void OnDrawGizmos()
-    {
-        if(PathLinks == null || PathLinks.Length == 0)
-        {
-            return;
-        }
-        for(int i = 1; i < PathLinks.Length; i++)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(PathLinks[i - 1].position, PathLinks[i].position);
-        }
-    }
+    ///// <summary>
+    ///// Отрисовывает путь в редакторе
+    ///// </summary>
+    //public void OnDrawGizmos()
+    //{
+    //    if(PathLinks == null || PathLinks.Length == 0)
+    //    {
+    //        return;
+    //    }
+    //    for(int i = 1; i < PathLinks.Length; i++)
+    //    {
+    //        Gizmos.color = Color.red;
+    //        Gizmos.DrawLine(PathLinks[i - 1].position, PathLinks[i].position);
+    //    }
+    //}
 }
