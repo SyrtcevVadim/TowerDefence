e1ba3fbce55986a37bfeb818f06051489132b86b
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-29 18:25:27 +0300

    Old level have become TestLevel. Every level is a scene with ObserverObject,GameManager, Paths and LevelField

--- Assets/Scripts/MovingPath.cs
+++ Assets/Scripts/MovingPath.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 
 public class MovingPath : MonoBehaviour
 {
-    public Transform[] PathLinks;           // Массив звеньев пути
+    public Transform[] PathLinks;           // Звенья пути
     public Transform this[int index]
     {
         get
         {
             return PathLinks[index];
+        }
+    }
+    public int Length
+    {
+        get
+        {
+            return PathLinks.Length;
         }
     }
 
---
+++ Assets/Scripts/NonMono/Colors.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Colors
+{
+    /// <summary>
+    /// Цвета для путей уровня. Каждый путь обозначается своим цветом
+    /// </summary>
+    public static Color[] colors = new Color[9] { Color.white, Color.black, Color.green, Color.yellow, Color.blue, Color.cyan, Color.magenta, Color.grey, Color.red};
+    
+}
