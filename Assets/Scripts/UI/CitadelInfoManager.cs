011658c8f58c050ba4cfb6dd4685634bbb4f4d4d
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-31 12:41:11 +0300

    CitadelInfoPanel changes it's color in dependence of citadel's hp

--- Assets/Scripts/UI/CitadelInfoManager.cs
+++ Assets/Scripts/UI/CitadelInfoManager.cs
@@ -1,24 +1,56 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 public class CitadelInfoManager : MonoBehaviour
 {
     [Space]
     [Header("»нформаци€ о цитадели игрока")]
     public TMP_Text CitadelHPLabel;
+    public Image citadelInfoPanel;
     public GameObject playerCitadel;
+    
+
+    [Space]
+    [Header("÷вета панели в зависимости от текущего уровн€ здоровь€ цитадели")]
+    [Tooltip("÷вет панели, когда уровень здоровь€ башни в пределах 80-100% от максимального")]
+    public Color HP80_100PercentageColor;
+    [Tooltip("÷вет панели, когда уровень здоровь€ башни в пределах 60-80% от максимального")]
+    public Color HP60_80PercentageColor;
+    [Tooltip("÷вет панели, когда уровень здоровь€ башни в пределах 40-60% от максимального")]
+    public Color HP40_60PercentageColor;
+    [Tooltip("÷вет панели, когда уровень здоровь€ башни в пределах 20-40% от максимального")]
+    public Color HP20_40PercentageColor;
+    [Tooltip("÷вет панели, когда уровень здоровь€ башни в пределах 0-20% от максимального")]
+    public Color HP0_20PercentageColor;
 
     private void Start()
     {
         playerCitadel = GameObject.FindGameObjectsWithTag("Citadel")[0];
     }
-    private void Update()
+    public void UpdateCitadelHPInfo(float currentCitadelHP, float maxCitadelHP)
     {
-        //TODO: —делать более оптимально
-        UpdateCitadelHPInfo();
-    }
-    public void UpdateCitadelHPInfo()
-    {
-        CitadelHPLabel.text = playerCitadel.GetComponent<Citadel>().CurrentHP.ToString();
+        int percentage = (int)((currentCitadelHP / maxCitadelHP) * 100);
+        print(percentage);
+        if((80 <= percentage) && (percentage <=100))
+        {
+            citadelInfoPanel.color = HP80_100PercentageColor;
+        }
+        else if((60 <= percentage) && (percentage <= 80))
+        {
+            citadelInfoPanel.color = HP60_80PercentageColor;
+        }
+        else if((40 <= percentage) && (percentage <=60))
+        {
+            citadelInfoPanel.color = HP40_60PercentageColor;
+        }
+        else if((20 <= percentage) && (percentage <=40))
+        {
+            citadelInfoPanel.color = HP20_40PercentageColor;
+        }
+        else if((0 <=percentage) && (percentage <=20))
+        {
+            citadelInfoPanel.color = HP0_20PercentageColor;
+        }
+        CitadelHPLabel.text = currentCitadelHP.ToString();
     }
 }
