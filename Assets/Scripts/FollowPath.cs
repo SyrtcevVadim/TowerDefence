77463426ff6bce9b3616b3362317a9c286de215b
SyrtcevVadim <74814717+SyrtcevVadim@users.noreply.github.com>
2021-01-31 10:58:19 +0300

    Broad movement of enemies along to trajectory

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -8,13 +8,19 @@ public class FollowPath : MonoBehaviour
     [Tooltip("Скорость передвижения")]
     public int MovementSpeed;           // Скорость движения
     [Tooltip("Флаг, показывающий, может ли объект совершать перемещение в данный момент времени")]
-    //public bool CanMove;
     private int currentLinkIndex;
+    private Vector3 offsetVector;              // Смещение движения юнита относительно траектории пути
+    private float offsetX;               // Смещение юнита относительно пути по оси OX
+    private float offsetZ;               // Смещение юнита относительно пути по оси OZ
+
     public void Start()
     {
+        offsetX = Random.Range(-Constants.TERRAIN_CELL_SIZE / 2, Constants.TERRAIN_CELL_SIZE / 2);
+        offsetZ = Random.Range(-Constants.TERRAIN_CELL_SIZE / 2, Constants.TERRAIN_CELL_SIZE / 2);
+        offsetVector = new Vector3(offsetX, 0, offsetZ);
         currentLinkIndex = 0;
-        transform.position = Path[currentLinkIndex].position;
-    }
+        transform.position = Path[currentLinkIndex].position + offsetVector;
+    } 
 
     public void Update()
     {
@@ -22,9 +28,9 @@ public class FollowPath : MonoBehaviour
         {
             return;     // Объект пришел к цитадели
         }
-        transform.position = Vector3.MoveTowards(transform.position, Path[currentLinkIndex + 1].position, Time.deltaTime * MovementSpeed) ;
+        transform.position = Vector3.MoveTowards(transform.position, Path[currentLinkIndex + 1].position + offsetVector, Time.deltaTime * MovementSpeed) ;
 
-        float remainDistance = (Path[currentLinkIndex + 1].position - transform.position).magnitude;
+        float remainDistance = (Path[currentLinkIndex + 1].position +offsetVector - transform.position).magnitude;
         if (remainDistance < 0.01f)
         {
             currentLinkIndex++;
